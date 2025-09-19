6a80c8d0b0ca29ada35738aee146f52684b1e8af
Br0cod3 <thespammingbott@gmail.com>
2025-09-19 20:34:48 +0100

    implement: colorArray

--- src/logic.js
+++ src/logic.js
@@ -5,3 +5,5 @@ class Project {
     this.storage = []
   }
 }
+export const colorArray = ["#E30808", "#08EA81", "#0887E3", "#6CBC0C", "#400CBC", "#BCA10C", "#F51400", "#054166", "#0CBC26", "#0C95BC", "#900CBC", "#BC0C0C", "#08EA81"]
+
