feacab03d276fee0ce3be6feb29f993bdbcf662c
Br0cod3 <thespammingbott@gmail.com>
2025-09-19 20:38:14 +0100

    implement: createElement Function

--- src/Dom.js
+++ src/Dom.js
@@ -2,3 +2,11 @@ import TrashIcon from "../svg/Trash.svg";
 import { projects, colorArray } from "./logic.js"
 
 let currentProject = null;
+
+function createElement(tag, className, textContent) {
+  const el = document.createElement(tag);
+  if (className) el.className = className;
+  if (textContent) el.textContent = textContent;
+
+  return el;
+}
